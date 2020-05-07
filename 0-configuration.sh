42480b24ea96250444c6b37e7c98e039a70fad9a
EduardMisa <eduardo@tirsolutions.com>
2020-05-07 12:59:25 +0800

    initial commit

---
+++ 0-configuration.sh
@@ -0,0 +1,4 @@
+export DEPLOYMENT_PATH="/var/www/core3-grpc-client.u4rdsystem.com"
+export GIT_LINK="https://eduardmisa@github.com/eduardmisa/CORE3-gRPC-RMSContext-Client.git"
+export GIT_BRANCH="master"
+export GIT_PASSWORD="531ed96864Z"
---
+++ RMSContext.GRPC.Consumer/Controllers/ApplicationController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace RMSContext.GRPC.Consumer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicationController : ControllerBase
+    {
+        string grpcServer;
+        public ApplicationController(IConfiguration configuration)
+        {
+            grpcServer = configuration.GetSection("ApplicationSettings").GetValue<string>("GRPCS_SERVER_HOST");
+        }
+
+        // GET: api/Application
+        [HttpGet]
+        public ListResponse Get()
+        {
+            // The port number(5001) must match the port of the gRPC server.
+            using var channel = GrpcChannel.ForAddress(grpcServer);
+            var client = new ApplicationRPC.ApplicationRPCClient(channel);
+
+            var test = client.List(new ListRequest());
+
+            return test;
+        }
+
+        // GET: api/Application/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Application
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Application/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
