9cd1ef0b03ef04eb958a50b7b49abfb59e825b22
eduardmisa <eduardmisa@gmail.com>
2020-07-01 11:37:18 +0800

    domain rename

--- 0-configuration.sh
+++ 0-configuration.sh
@@ -1,4 +1,4 @@
-export DEPLOYMENT_PATH="/var/www/core3-grpc-client.u4rdsystem.com"
+export DEPLOYMENT_PATH="/var/www/core3-grpc-client.emisa.dev"
 export GIT_LINK="https://eduardmisa@github.com/eduardmisa/CORE3-gRPC-RMSContext-Client.git"
 export GIT_BRANCH="master"
 export GIT_PASSWORD="531ed96864Z"
